85fe5de5ff8299c3219fe596b22b989ee39a6470
Matt Levy <milevy1@gmail.com>
2019-06-25 21:19:22 -0600

    Adds uniqueness requirement for User emails

--- migrations/20190624214620-create-user.js
+++ migrations/20190624214620-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,7 +9,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       password_digest: {
         type: Sequelize.STRING
