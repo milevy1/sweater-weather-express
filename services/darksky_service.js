c6818853089ad7c505fb44550b895e613656d5f0
Matt Levy <milevy1@gmail.com>
2019-06-30 12:16:09 -0600

    Adds DarkSkyService which takes longitude/latitude and makes an API call for weather data

---
+++ services/darksky_service.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+module.exports = class DarkSkyService {
+  constructor(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.getData();
+  }
+
+  async getData() {
+    let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${this.latitude},${this.longitude}`)
+    this.data = await response.json()
+  }
+}
--- services/index.js
+++ services/index.js
@@ -1,5 +1,7 @@
 const GoogleMapsService = require("./google_maps_service");
+const DarkSkyService = require("./darksky_service");
 
 module.exports = {
-  GoogleMapsService: GoogleMapsService
+  GoogleMapsService: GoogleMapsService,
+  DarkSkyService: DarkSkyService
 }
